bf3c10175ca83c9a61bb8f7f02d0e84253bb29b8
jero98772 <jero98772@pop-os.localdomain>
2023-08-12 16:38:50 -0500

    add translation function

--- backend/core/main.py
+++ backend/core/main.py
@@ -2,15 +2,20 @@
 # -*- coding: utf-8 -*- 
 #PrimusChat - by MLEAFIT
 from flask import Flask, render_template, request, flash, redirect ,session,jsonify
+from core.tools.tools import *
+
 app = Flask(__name__)
 class webpage():
   @app.route("/")
   def index():
     return render_template("index.html")
+  #@app.route('/api/receive_data', methods=['POST'])
   @app.route('/api/receive_data', methods=['POST'])
   def receive_data():
     data = request.json
-    #new_data=tanslate(data)#later you will code send in the return 
+    print(data)
+    #print(data["text"],data["text"])
+    data= webTranslate(data["text"],data["lang"][:2],data["targetLang"][:2])
     print(data)
     #in frontend in console you will obtein "Data received successfully!"
-    return jsonify({"message": "Data received successfully!"})
+    return jsonify({"message": data})
